ce9c65d38baa95a976ef4a7170dc88d77e54339b
DESKTOP-ESNC61C\FPT <anhnh133@fpt.com>
2024-04-19 14:32:21 +0700

    first commit

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,96 @@
+import React from "react";
+import Colors from "../utils/common/color.ultil";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Controller, useForm } from "react-hook-form";
+
+const LoginScreen = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>LoginScreen</Text>
+        <View>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Email"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="email"
+          />
+          {errors.email && <Text style={styles.textRequired}>Email is required.</Text>}
+
+          <Controller
+            control={control}
+            rules={{
+              maxLength: 100,
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Password"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="password"
+          />
+          {errors.password && <Text style={styles.textRequired}>Password is required.</Text>}
+
+
+          <Button title="Submit" onPress={handleSubmit(onSubmit)} style={styles.btnSubmit} />
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    justifyContent: "center",
+    padding: 10,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: "500",
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: Colors.orange,
+    textAlign: "center",
+  },
+  textRequired: {
+    color: Colors.danger,
+  },
+  btnSubmit: {
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+});
+
+export default LoginScreen;
---
+++ src/utils/common/color.ultil.js
@@ -0,0 +1,18 @@
+const Colors = {
+    white: "#FFFFFF",
+    black: "#000000",
+    primary: "#007bff",
+    secondary: "#6c757d",
+    success: "#28a745",
+    danger: "#dc3545",
+    warning: "#ffc107",
+    info: "#17a2b8",
+    light: "#f8f9fa",
+    dark: "#343a40",
+    transparent: "transparent",
+    muted: "#6c757d",
+    orange: "#FFA500",
+    border: "#dcdcdc",
+};
+
+export default Colors;
