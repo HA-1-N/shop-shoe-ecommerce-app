1ca0f3e4a4cca73179b107614a51e0af289d8b38
DESKTOP-ESNC61C\FPT <anhnh133@fpt.com>
2024-06-06 17:51:48 +0700

    fix: middleware redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterSlice from './features/counterSlice'
-import { authReducer } from './features/auth.slice'
-import { cartReducer } from './features/cart.slice'
-import productSlice from './features/product.slice';
-
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./features/counterSlice";
+import { authReducer } from "./features/auth.slice";
+import { cartReducer } from "./features/cart.slice";
+import productSlice from "./features/product.slice";
 
 export const store = configureStore({
   reducer: {
@@ -12,4 +11,8 @@ export const store = configureStore({
     cart: cartReducer,
     product: productSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
